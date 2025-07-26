87031f622cbcf2c717acf81a7899e8fd9498223d
SviatoslavPotapenko <Sviatoslav.Potapenko@lnu.edu.ua>
2025-07-26 16:31:54 +0300

    app.js small changes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/"))); // Додай у app.js після app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true })); // Для сумісності з формами
 
 // Ендпоінт для продуктів (використовуємо db.pool)
